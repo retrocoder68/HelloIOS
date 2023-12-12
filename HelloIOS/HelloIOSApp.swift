546a2044ad510c74dc1a9638daa083209f4fcbef
skywalker <j.karlsson@retrocoder.se>
2023-12-12 14:55:07 +0100

    Initial Commit

---
+++ HelloIOS/HelloIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloIOSApp.swift
+//  HelloIOS
+//
+//  Created by Jörgen on 2023-12-12.
+//
+
+import SwiftUI
+
+@main
+struct HelloIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
